fe528b265f2fa00d8ee79f91a2a269ff299ad342
Peder <Peder.sanden@ntnu.no>
2023-12-05 23:21:28 +0100

    update features and styling

--- secretsanta/script.js
+++ secretsanta/script.js
@@ -1,8 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
     const pickButton = document.getElementById('pickButton');
     const resultDiv = document.getElementById('result');
+    const allNamesDiv = document.getElementById('allNames');
 
     pickButton.addEventListener('click', pickSecretSanta);
+
+    // Initial display of all names when the page loads
+    fetchAndDisplayAllNames();
 
     function pickSecretSanta() {
         fetch('server.php', {
@@ -14,7 +18,7 @@ document.addEventListener('DOMContentLoaded', function () {
             .then(response => response.json())
             .then(names => {
                 if (names.length === 0) {
-                    resultDiv.innerHTML = '\nAlle deltakere er trukket. God Jul!';
+                    resultDiv.innerHTML = '<br>Alle deltakere er trukket. God Jul!';
                 } else {
                     const randomIndex = Math.floor(Math.random() * names.length);
                     const selectedName = names[randomIndex];
@@ -23,23 +27,97 @@ document.addEventListener('DOMContentLoaded', function () {
 
                     if (isPresent) {
                         // Notify the server to remove the selected name
-                        fetch('server.php', {
-                            method: 'POST',
-                            headers: {
-                                'Content-Type': 'application/json',
-                            },
-                            body: JSON.stringify({ nameToRemove: selectedName }),
-                        })
-                            .then(response => response.json())
-                            .then(() => {
-                                resultDiv.innerHTML = `\n${selectedName} har blitt trukket! \nKos deg med gaven!`;
-                            })
-                            .catch(error => console.error('Error notifying server:', error));
+                        removeName(selectedName);
                     } else {
                         pickSecretSanta(); // Retry if the person is not present
                     }
                 }
             })
             .catch(error => console.error('Error fetching names from server:', error));
+    }
+
+    function removeName(nameToRemove) {
+        fetch('server.php', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ nameToRemove }),
+        })
+            .then(response => response.json())
+            .then(() => {
+                resultDiv.innerHTML = `<br>${nameToRemove} har blitt trukket!<br> Kos deg med gaven!`;
+                // Refresh the display after removing a name
+                fetchAndDisplayAllNames();
+            })
+            .catch(error => console.error('Error notifying server:', error));
+    }
+
+    function fetchAndDisplayAllNames() {
+        fetch('server.php', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+            .then(response => response.json())
+            .then(names => {
+                displayAllNames(names);
+            })
+            .catch(error => console.error('Error fetching names from server:', error));
+    }
+
+    function displayAllNames(names) {
+        // Clear the names display
+        allNamesDiv.innerHTML = '';
+
+        // Separate identical and different names
+        const { identicalNames, differentNames } = separateIdenticalNames(names || []);
+
+        // Display identical names vertically
+        identicalNames.forEach(identicalGroup => {
+            const namesList = document.createElement('ul');
+            identicalGroup.forEach(name => {
+                const listItem = document.createElement('li');
+                listItem.textContent = name;
+                namesList.appendChild(listItem);
+            });
+            allNamesDiv.appendChild(namesList);
+        });
+
+        // Display different names side by side
+        const namesList = document.createElement('ul');
+        differentNames.forEach(name => {
+            const listItem = document.createElement('li');
+            listItem.textContent = name;
+            namesList.appendChild(listItem);
+        });
+        allNamesDiv.appendChild(namesList);
+    }
+
+    function separateIdenticalNames(names) {
+        const groupedNames = {};
+
+        // Group names by their value
+        names.forEach(name => {
+            if (!groupedNames[name]) {
+                groupedNames[name] = [];
+            }
+            groupedNames[name].push(name);
+        });
+
+        // Separate identical and different names
+        const identicalNames = [];
+        const differentNames = [];
+
+        Object.values(groupedNames).forEach(group => {
+            if (group.length > 1) {
+                identicalNames.push(group);
+            } else {
+                differentNames.push(group[0]);
+            }
+        });
+
+        return { identicalNames, differentNames };
     }
 });
--- secretsanta/server.php
+++ secretsanta/server.php
@@ -1,46 +1,39 @@
 <?php
 
-$logFilePath = 'var/log/ss/remove_log.txt';
+$jsonFile = 'names.json';
+$removeLogFile = 'var/log/remove_log.txt';
 
-function logMessage($message) {
-    global $logFilePath;
-    $timestamp = date('Y-m-d H:i:s');
-    $ipAddress = $_SERVER['REMOTE_ADDR'];
-    $userAgent = isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : 'Unknown';
+// Load names from the JSON file
+$names = json_decode(file_get_contents($jsonFile), true);
 
-    $logMessage = "$timestamp - IP: $ipAddress, User-Agent: $userAgent - $message\n";
-    file_put_contents($logFilePath, $logMessage, FILE_APPEND);
-}
-
-if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    // Handle GET request to retrieve names
-    $names = json_decode(file_get_contents('names.json'), true);
-    echo json_encode($names);
-} elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Handle POST request to remove a name
+// Handle POST request to remove a name
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $postData = json_decode(file_get_contents('php://input'), true);
 
     if (isset($postData['nameToRemove'])) {
         $nameToRemove = $postData['nameToRemove'];
 
-        $names = json_decode(file_get_contents('names.json'), true);
+        // Remove only one instance of the selected name
+        $index = array_search($nameToRemove, $names);
+        if ($index !== false) {
+            array_splice($names, $index, 1);
+        }
 
-        // Remove the specified name
-        $names = array_values(array_diff($names, [$nameToRemove]));
+        // Save the updated names back to the JSON file
+        file_put_contents($jsonFile, json_encode($names));
 
-        // Update the JSON file
-        file_put_contents('names.json', json_encode($names, JSON_PRETTY_PRINT));
+        // Log the removed name to remove_log.txt
+        file_put_contents($removeLogFile, date('Y-m-d H:i:s') . ' - ' . $nameToRemove . PHP_EOL, FILE_APPEND);
 
-        // Log the removal
-        logMessage("Removed name: $nameToRemove");
-
+        // Return a response (you might want to customize this based on your needs)
         echo json_encode(['success' => true]);
-    } else {
-        http_response_code(400);
-        echo json_encode(['error' => 'Invalid request']);
+        exit;
     }
-} else {
-    http_response_code(405);
-    echo json_encode(['error' => 'Method Not Allowed']);
 }
-?>
+
+// Handle GET request to fetch all names
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    header('Content-Type: application/json');
+    echo json_encode($names);
+    exit;
+}
